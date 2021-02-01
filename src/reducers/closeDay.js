9690b491b1c1053f4f4989048bf834e0ef9f5aad
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 23:15:31 +0100

    Début exercice étape 18 : combinaison des réducteurs de niveau racine

--- src/reducers/closeDay.js
+++ src/reducers/closeDay.js
@@ -1,5 +1,7 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
 
+import { isoDate } from '../lib/helpers'
+
 // Types d’actions
 // ---------------
 
@@ -10,6 +12,27 @@ export const closeDay = createAction('goal-tracker/closeDay/closeDay')
 
 export default createReducer({}, (builder) => {
   builder.addCase(closeDay, (state, action) => {
-    // FIXME
+    state.history = tallyPreviousDay(state)
+    state.today = isoDate(new Date())
+    state.todaysProgress = {}
   })
 })
+
+// Calcul de l’historisation
+// -------------------------
+
+function tallyPreviousDay({ goals, history, today, todaysProgress }) {
+  const progresses = {}
+  for (const { id, target } of goals) {
+    const progress = todaysProgress[id] || 0
+    if (progress > 0) {
+      progresses[id] = [progress, target]
+    }
+  }
+
+  if (Object.keys(progresses).length === 0) {
+    return history
+  }
+
+  return [{ date: today, progresses }, ...history]
+}
